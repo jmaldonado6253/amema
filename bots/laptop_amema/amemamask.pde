818b2e514c91e89cb6dd41769b73ac59fd709059
zak <zziebell@risd.edu>
2016-01-02 18:57:59 -0600

    Added processing sketch to generate threshold

---
+++ bots/laptop_amema/amemamask.pde
@@ -0,0 +1,59 @@
+PImage source;       // Source image
+PImage destination;  // Destination image
+
+int linearize(int x, int y)
+{
+  x = constrain(x, 0, width - 1);
+  y = constrain(y, 0, height - 1);
+ 
+  return x + y * width;
+}
+
+float g_time;
+
+void setup() {
+  size(400, 400);
+  source = loadImage("input.jpg");  
+  // The destination image is created as a blank image the same size as the source.
+  destination = createImage(source.width, source.height, RGB);
+    g_time = 0.0;
+}
+
+void draw() {  
+   //g_time += 0.1;
+   noiseDetail(2,1);
+  float threshold = 200;
+  float threshold2 = 250;
+     g_time += 0.1;
+  // We are going to look at both image's pixels
+  source.loadPixels();
+  destination.loadPixels();
+  
+  for (int x = 0; x < source.width; x++) {
+    for (int y = 0; y < source.height; y++ ) {
+   
+     float noiseval = noise(g_time / 1.0, float(x) / 100.0, float(y) / 100.0);
+     
+      int loc = x + y*source.width;
+      // Test the brightness against the threshold
+      if (brightness(source.pixels[loc]) > threshold2*noiseval) {
+        destination.pixels[loc]  = color(0);  // White
+      }   else if ((brightness(source.pixels[loc]) < (threshold2*noiseval)) && ((brightness(source.pixels[loc])) > threshold*noiseval)) {
+        destination.pixels[loc]  = source.pixels[loc];    // middle
+      }
+      else {
+        destination.pixels[loc]  = color(255);    // Black
+      }
+    }
+  }
+
+  // We changed the pixels in destination
+  destination.updatePixels();
+  // Display the destination
+  image(destination,0,0);
+  
+  
+  
+  
+   save("output.jpg");
+}
---
+++ bots/laptop_amema/index.js
@@ -0,0 +1,110 @@
+//
+//  AmemaBot
+//  Service to periodically get, modify and post images
+//
+var fs = require('fs');
+var gm = require('gm');
+var Twit = require('twit');
+var async = require('async');
+var shortid = require('shortid');
+//var config = require('./config');
+//var T = new Twit(config);
+
+//var dbpath = './amemadb.json';
+
+//var db = JSON.parse(fs.readFileSync(dbpath, 'utf8'));
+
+//TODO get and modify images
+
+/*
+var curimgid = shortid.generate();
+function createnewimg(curimgid) {
+  db[curimgid] = {};
+  db[curimgid].author = 'testbot';
+  db[curimgid].origin = Date.now();
+  db[curimgid].history = [{date: Date.now(), author: 'testbot'}];
+}
+*/
+
+/*
+fs.writeFile(dbpath, JSON.stringify(db, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + dbpath);
+      }
+});
+
+*/
+
+gm('test.jpg')
+.size(function (err, size) {
+  if (!err)
+    console.log(size.width > size.height ? 'wider' : 'taller than you');
+});
+
+
+//  var exec = require('child_process').exec;
+//  var cmd = "/home/javier/badpng/badpng \
+//  /home/javier/amema/images/used/#{img} \==
+//  /home/javier/amema/images/used/#{outputname}.png";
+
+// exec(cmd, function(error, stdout, stderr) {
+  // command output is in stdout
+// });
+// }
+
+//
+//  post a tweet
+//
+
+/*postTweet = function (status, callback) {
+  var b64content = fs.readFileSync('/Users/Zak/Documents/amema\ node/node_modules/twit/test.jpeg', { encoding: 'base64' });
+  console.log("file read in");
+  T.post(
+    'media/upload',
+    { media_data: b64content },
+    function (err, data, response) {
+      var mediaIdStr = data.media_id_string;
+      var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] };
+
+      T.post('statuses/update', params, function (err, data, response) {
+        console.log(data);
+      });
+      console.log("something should have happened by now");    });
+};
+*/
+
+//function randIndex (arr) {
+//  var index = Math.floor(arr.length*Math.random());
+//  return arr[index];
+//}
+
+
+////the big daddy function, which contains in its waterfall all the lil functions
+
+// run = function() {
+//  async.waterfall([
+//    getImage
+//    modifyImage
+//  ],
+//  function(err, botData) {
+//    if (err) {
+//      console.log('There was an error posting to Twitter: ', err);
+//    } else {
+//      console.log('Tweet successful!');
+//      console.log('Tweet: ', botData.tweetBlock);
+//    }
+//    console.log('Base tweet: ', botData.baseTweet);
+//  });
+ // }
+
+
+// setInterval(function() {
+//  try {
+//    run();
+//  }
+//  catch (e) {
+//    console.log(e);
+//  }
+// }, 60000 * 60);
